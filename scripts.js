a4110bb0966d26ccf73dfcb9956235e60fdcc4ee
NitaLewska <Nita@ya.ru>
2021-07-17 23:00:51 +0300

    Updated JS

--- scripts.js
+++ scripts.js
@@ -5,11 +5,12 @@ const newUser = {
     password: undefined
 };
 
+
 document.getElementById("submitForm").addEventListener('click', validation);
 
 function validation(e) {
     e.preventDefault();
-
+    let check = 0;
     newUser.firstName = document.getElementById("firstName").value;
     newUser.lastName = document.getElementById("lastName").value;
     newUser.email = document.getElementById("email").value;
@@ -20,7 +21,8 @@ function validation(e) {
         document.getElementById("firstName").classList.add('error')
     } else {
         document.getElementById("firstNameError").classList.add('hidden');
-        document.getElementById("firstName").classList.remove('error')
+        document.getElementById("firstName").classList.remove('error');
+        check++;
     }
 
     if (newUser.lastName.length < 1) {
@@ -28,7 +30,8 @@ function validation(e) {
         document.getElementById("lastName").classList.add('error')
     } else {
         document.getElementById("lastNameError").classList.add('hidden');
-        document.getElementById("lastName").classList.remove('error')
+        document.getElementById("lastName").classList.remove('error');
+        check++;
     }
 
     if (newUser.password.length < 1) {
@@ -36,7 +39,8 @@ function validation(e) {
         document.getElementById("password").classList.add('error')
     } else {
         document.getElementById("passwordError").classList.add('hidden');
-        document.getElementById("password").classList.remove('error')
+        document.getElementById("password").classList.remove('error');
+        check++;
     }
 
     if (newUser.password.length < 1) {
@@ -44,7 +48,8 @@ function validation(e) {
         document.getElementById("password").classList.add('error')
     } else {
         document.getElementById("passwordError").classList.add('hidden');
-        document.getElementById("password").classList.remove('error')
+        document.getElementById("password").classList.remove('error');
+        check++;
     }
 
     const emailReg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -54,8 +59,14 @@ function validation(e) {
         document.getElementById("email").classList.add('error')
     } else {
         document.getElementById("emailError").classList.add('hidden');
-        document.getElementById("email").classList.remove('error')
+        document.getElementById("email").classList.remove('error');
+        check++;
     }
 
-    
+    if (check = 5) {
+        document.getElementById("firstName").value = "";
+        document.getElementById("lastName").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("password").value = "";
+    }
 }
